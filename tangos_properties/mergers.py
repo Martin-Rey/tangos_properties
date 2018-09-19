7d527b1e9a1b02a44b081886ce339079ed9e8eb3
Martin-Rey <mrey@imac7.star.ucl.ac.uk>
2018-09-19 11:50:58 +0100

    Adding number of major mergers calculations

--- tangos_properties/mergers.py
+++ tangos_properties/mergers.py
@@ -1,35 +1,75 @@
 from tangos.properties import LivePropertyCalculation
+from tangos.live_calculation import NoResultsError
+from tangos.properties import PropertyCalculation
+from tangos import relation_finding
 
 
-class Mergers(LivePropertyCalculation):
+class Mergers(PropertyCalculation):
     requires_particle_data = False
-    names = "mergers_z, mergers_r, mergers_halos"
+    names = "mergers_z", "mergers_ratios"
+
+    @classmethod
+    def no_proxies(self):
+        return True
 
     def calculate(self, _, existing_properties):
         return self._get_mergers_of_major_progenitor(existing_properties)
 
     @staticmethod
     def _get_mergers_of_major_progenitor(input_halo):
-        """Given a halo, return the redshifts and ratios of all mergers on the major progenitor branch
-
-        :parameter input_halo - the halo to consider the history of
-        :type input_halo tangos.core.Halo
-
-        :returns redshift, ratio, halo - arrays of the redshifts, ratios (1:X) and halo DB objects for the mergers
-        """
         import numpy as np
         redshift = []
         ratio = []
-        halo = []
         while input_halo is not None:
-            mergers = tangos.relation_finding.MultiHopMostRecentMergerStrategy(input_halo).all()
+            mergers = relation_finding.MultiHopMostRecentMergerStrategy(input_halo).all()
             if len(mergers) > 0:
                 for m in mergers[1:]:
                     redshift.append(mergers[0].timestep.next.redshift)
-                    halo.append((mergers[0], m))
                     ratio.append(float(mergers[0].NDM) / m.NDM)
                 input_halo = mergers[0]
             else:
                 input_halo = None
 
-        return np.array(redshift), np.array(ratio), halo
+        return np.array(redshift), np.array(ratio)
+
+
+class NumberMajorMergers(LivePropertyCalculation):
+    requires_particle_data = False
+    names = "number_major_mergers"
+
+    def calculate(self, _, halo):
+        import numpy as np
+        ratios = halo['mergers_ratios']
+        major_mergers = np.where(ratios <= self._get_major_merger_ratio_def())[0]
+        return len(major_mergers)
+
+
+    @staticmethod
+    def _get_major_merger_ratio_def():
+        return 4
+
+    def requires_property(self):
+        return ["mergers_ratios"]
+
+
+class RedshiftLastMajorMergers(LivePropertyCalculation):
+    requires_particle_data = False
+    names = "z_last_major_mergers"
+
+    def calculate(self, _, halo):
+        import numpy as np
+        ratios = halo['mergers_ratios']
+
+        if len(ratios) == 0:
+            return 0.0
+        else:
+            major_mergers = np.where(ratios <= self._get_major_merger_ratio_def())
+            return len(major_mergers)
+
+
+    @staticmethod
+    def _get_major_merger_ratio_def():
+        return 4
+
+    def requires_property(self):
+        return ["mergers_z", "mergers_ratios"]
